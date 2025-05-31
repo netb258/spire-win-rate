4a7e47ac259ab52e07b5139844cf517ab0f5f2a1
netb258 <netb258@abv.bg>
2025-05-31 18:35:54 +0300

    Update project.clj

--- project.clj
+++ project.clj
@@ -1,1 +1,12 @@
-
+(defproject spire-win-rate "0.1.0-SNAPSHOT"
+  :description "FIXME: write description"
+  :url "http://example.com/FIXME"
+  :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
+            :url "https://www.eclipse.org/legal/epl-2.0/"}
+;;  :plugins [[cider/cider-nrepl "0.49.0"]]
+  :dependencies [[org.clojure/clojure "1.11.1"]
+                 [cheshire "5.13.0"]]
+  :main ^:skip-aot spire-win-rate.core
+  :target-path "target/%s"
+  :profiles {:uberjar {:aot :all
+                       :jvm-opts ["-Dclojure.compiler.direct-linking=true"]}})
